79421e7da78d119f4ec01af894c98ca836d61055
Craig Steele <csteele23@Craigs-MacBook-Pro.local>
2017-10-21 19:22:54 -0400

    changed to ajax

--- app/routes/greggames/gamelibrary.js
+++ app/routes/greggames/gamelibrary.js
@@ -5,17 +5,33 @@ export default Ember.Route.extend({
     games: null,
     init() {
         var that = this;
-        Ember.$.getJSON(ENV.APP.API_HOST+"/greggames", function (data) {
+        // Ember.$.getJSON(ENV.APP.API_HOST+"/greggames", function (data) {
 
+        //     console.log(ENV.APP.API_HOST);
+        //     data = data.map(v => v.toLowerCase());
+        //     that.set("games",data);
+        //     that.refresh();
+
+
+
+
+
+
+        // });
+
+        var request = Ember.$.ajax({
+            type: 'GET',
+            url: ENV.APP.API_HOST + "/greggames",
+            dataType: "json",
+            xhrFields: {
+                withCredentials: true
+            }
+        });
+        request.done(function (data) {
             console.log(ENV.APP.API_HOST);
             data = data.map(v => v.toLowerCase());
-            that.set("games",data);
+            that.set("games", data);
             that.refresh();
-
-       
-
-
-
 
         });
 
